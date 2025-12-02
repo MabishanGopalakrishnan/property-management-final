3176657c108164234bd4b1ad96d978ce57614d89
MabishanGopalakrishnan <114019465+MabishanGopalakrishnan@users.noreply.github.com>
2025-12-02 01:12:06 -0500

    working backend error with google login and need to work on pytest also check all possible error that we could run into

--- backend/add_maintenance_fields.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "MaintenanceRequest" ADD COLUMN IF NOT EXISTS contractor TEXT;
-ALTER TABLE "MaintenanceRequest" ADD COLUMN IF NOT EXISTS photos TEXT[] DEFAULT '{}';
--- backend/src/routes/paymentHistoryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getPaymentsByLease } from "../controllers/paymentHistoryController.js";
-
-const router = express.Router();
-
-router.get("/:leaseId", getPaymentsByLease);
-
-export default router;
